4bb6851681a8542b6a2c4f729e5a109bea015742
JohnXie <xie_jj@yahoo.com>
2022-03-30 15:56:03 +0000

    update test.py and .replit

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+
+from random import seed
+from random import random
+# seed random number generator
+seed(1)
+# generate some random numbers
+print(random(), random(), random())
+seed(1)
+print(random(), random(), random())
+print(random(), random(), random())
 a = 1
 b = 3
 c = a+b
