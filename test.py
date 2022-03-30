c76f100145b84c131a16c7868a1c48aab81c8ba3
JohnXie <xie_jj@yahoo.com>
2022-03-30 16:12:18 +0000

    pyproject.toml

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-
 from random import seed
 from random import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 4, 8, 16])
+
+plt.savefig('plot.png')
+
 # seed random number generator
 seed(1)
 # generate some random numbers
@@ -10,5 +16,7 @@ print(random(), random(), random())
 print(random(), random(), random())
 a = 1
 b = 3
-c = a+b
+c = a + b
 print(c)
+array1 = np.array([1, 2, 3, 4, 5])
+print(array1, array1[0])
